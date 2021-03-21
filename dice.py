ca49af3c412d388c5b3004d7d31a4b675f688f35
Yure Barbosa Couto <49762865+yurecouto@users.noreply.github.com>
2021-03-21 09:59:35 -0300

    commit 2

--- dice.py
+++ dice.py
@@ -1,4 +1,13 @@
+from random import randint]
+
 def tittle(msg):
     print('-=' * 30)
     print(f'{msg:^70}')
     print('-=' * 30)
+
+
+class Dice:
+    def __init__(self, type):
+        self.type = type
+
+        return randint(0, type)
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from dice import *
 
 tittle('\033[1;31mDICE SIMULATOR\033[m')
+
+dado = Dice(6)
+
+print(dado)
