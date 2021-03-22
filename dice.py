1872d914058770d775bb68e510f8c5245f60d00e
Yure Barbosa Couto <49762865+yurecouto@users.noreply.github.com>
2021-03-21 23:06:45 -0300

    commit 5 21/03

--- dice.py
+++ dice.py
@@ -1,20 +1,41 @@
 from random import randint
 from time import sleep
 
+
 def tittle(msg):
     print('-=' * 30)
     print(f'{msg:^70}')
     print('-=' * 30)
 
 
-def dice(number):
-    dice_roll = randint(0, number)
-    print(f'Rollind the \033[1;32mD{number}\033[mdice')
+def dual_d6():
+    d1 = randint(1, 6)
+    d2 = randint(1, 6)
+    print(f'Rolling two \033[1;32mD6\033[m dice')
     sleep(0.5)
     print('...')
     sleep(0.5)
     print('..')
     sleep(0.5)
     print('.')
+    sleep(0.5)
+
+    print(f'>> {d1} <<')
+    sleep(0.5)
+    print(f'>> {d2} <<')
+    sleep(0.5)
+    print(f'The result is {d1 + d2}')
+
+
+def free_dice(number):
+    dice_roll = randint(1, number)
+    print(f'Rolling the \033[1;32mD{number}\033[m dice')
+    sleep(0.5)
+    print('...')
+    sleep(0.5)
+    print('..')
+    sleep(0.5)
+    print('.')
+    sleep(0.5)
 
     print(f'>>> {dice_roll} <<<')
--- main.py
+++ main.py
@@ -4,19 +4,56 @@ again = 'Y'
 dice_type = 0 
 
 tittle('\033[1;31mDICE SIMULATOR\033[m')
-print("Wellcome to the best dice simulator you've ever seen!\n")
+print("Welcome to the best dice simulator you've ever seen!\n")
 
 while True:
-    if again.upper() == 'N':
-        break
+    print('1 - Press "A" for the free dice game-mode.')
+    print('2 - or "B" for the dual D6 game-mode.')
+    print('3 - you can press "X" for quit.')
+    game_mode = input(f'Choose your game-mode [A, B or X]: ')
 
-    elif again.upper() == 'Y':
-        dice_type = int(input('Wich kind of dice you want to roll? '))
-        dice(dice_type)
+    if game_mode.upper() == 'A':
+        while True:
+            if again.upper() == 'N':
+                break
+
+            elif again.upper() == 'Y':
+                dice_type = int(input('Which kind of dice you want to roll? '))
+                free_dice(dice_type)
+
+            else:
+                print('\033[1;31mERROR\033[m')
+                print('You can only type "Y" for yes or "N" for not')
+
+            again = input('Play again? [y/n] ')
+
+    elif game_mode.upper() == 'B':
+        while True:
+            if again.upper() == 'N':
+                break
+
+            elif again.upper() == 'Y':
+                dual_d6()
+
+            else:
+                print('\033[1;31mERROR\033[m')
+                print('You can only type "Y" for yes or "N" for not')
+
+            again = input('Play again? [y/n] ')
+
+    elif game_mode.upper() == 'X':
+        break
 
     else:
         print('\033[1;31mERROR\033[m')
-        print('You can only type y for yes or n for not')
+        print('You can only type "A", "B" or "X"')
 
-
-    again = input('Play again? [y/n] ')
+print('Closing')
+sleep(0.5)
+print('...')
+sleep(0.5)
+print('..')
+sleep(0.5)
+print('.')
+sleep(0.5)
+print('\033[1;31mTHE END.\033[m')
