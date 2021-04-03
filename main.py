fd63cae8de56203f8017a867bbd21c093b03a4bd
Yure Barbosa Couto <49762865+yurecouto@users.noreply.github.com>
2021-04-03 19:01:04 -0300

    bugfix 03/04

--- main.py
+++ main.py
@@ -1,47 +1,46 @@
 from dice import *
 
-again = 'Y'
 dice_type = 0 
 
 tittle('\033[1;31mDICE SIMULATOR\033[m')
 print("Welcome to the best dice simulator you've ever seen!\n")
 
-# There's a bug in the following while, you can't play another mode when you've already played one mode
-
 while True:
+    again = 'Y'
     print('1 - Press "A" for the free dice game-mode.')
     print('2 - or "B" for the dual D6 game-mode.')
     print('3 - you can press "X" for quit.')
     game_mode = input(f'Choose your game-mode [A, B or X]: ')
 
-    if game_mode.upper() == 'A':
+    while game_mode.upper() == 'A':
         
-        while again.upper() == 'Y':
+        dice_type = int(input('Which kind of dice you want to roll? '))
+        free_dice(dice_type)
 
-            if again.upper() == 'Y':
-                dice_type = int(input('Which kind of dice you want to roll? '))
-                free_dice(dice_type)
+        again = input('Play again? [Y/N] ')
 
-            else:
-                print('\033[1;31mERROR\033[m')
-                print('You can only type "Y" for yes or "N" for not')
+        if again.upper() not in 'YN':
+            print('\033[1;31mERROR\033[m')
+            print('You can only type "Y" for yes or "N" for not')
 
-            again = input('Play again? [y/n] ')
+        if again.upper() == 'N':
+            break
 
-    elif game_mode.upper() == 'B':
 
-        while again.upper == 'Y':
-            
-            if again.upper() == 'Y':
-                dual_d6()
+    while game_mode.upper() == 'B':
 
-            else:
-                print('\033[1;31mERROR\033[m')
-                print('You can only type "Y" for yes or "N" for not')
+        dual_d6()
 
-            again = input('Play again? [y/n] ')
+        again = input('Play again? [y/n] ')
 
-    elif game_mode.upper() == 'X':
+        if again.upper() not in 'YN':
+            print('\033[1;31mERROR\033[m')
+            print('You can only type "Y" for yes or "N" for not')
+
+        if again.upper() == 'N':
+            break
+
+    if game_mode.upper() == 'X':
         break
 
     else:
@@ -50,3 +49,6 @@ while True:
 
 sleeping('Closing')
 print('\033[1;31mTHE END.\033[m')
+
+'''print('\033[1;31mERROR\033[m')
+            print('You can only type "Y" for yes or "N" for not')'''
