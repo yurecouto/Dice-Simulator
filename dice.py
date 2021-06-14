b89b1ac91b511bacd3930f541b58c5f2aca4b449
Yure Barbosa Couto <49762865+yurecouto@users.noreply.github.com>
2021-06-14 20:53:26 -0300

    OOP 02

--- dice.py
+++ dice.py
@@ -1,3 +1,11 @@
 from random import randint, choice
 from time import sleep
 
+class Dice:
+    def __init__(self, type=4):
+        self.type = type
+
+    def rolling(type):
+        roll = randint(1, type)
+        
+        print(roll)
